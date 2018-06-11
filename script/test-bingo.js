65db00771a6d028569f3e4ba4083ee7598dfe23b
YoshiRider <40186033+YoshiRider@users.noreply.github.com>
2018-06-11 17:19:20 -0600

    Add files via upload

---
+++ script/test-bingo.js
@@ -0,0 +1,183 @@
+var bingoList = [];
+bingoList[1] = [
+  { name: "Splatoon DLC", types: [""] },
+  { name: "New 2D Mario", types: [""] },
+  { name: "New 2D Zelda", types: [""] },
+  { name: "New Donkey Kong", types: [""] },
+  { name: "New Pikmin", types: [""] }
+];
+bingoList[2] = [
+  { name: "F-Zero", types: [""] },
+  { name: "Star Fox Reboot, Again", types: [""] },
+  { name: "Bayonetta", types: [""] },
+  { name: "WAHHHHH makes prominent role", types: ["wah"] }
+];
+bingoList[3] = [
+  { name: "Someone actually gets on stage", types: [""] },
+  { name: "Memes/Memeable quote", types: [""] },
+  { name: "New & Unique series", types: [""] },
+  { name: "Geno in Smash", types: [""] },
+  { name: "Smash Brothers", types: [""] }
+];
+bingoList[4] = [
+  { name: "Spend 20 minutes on one game then tease the rest", types: [""] },
+  { name: "Switch Ports", types: [""] },
+  { name: "More Nails in the Wii U's coffin", types: [""] },
+  { name: "Online Classic Games explained", types: [""] },
+  { name: "Reggie murders another one of his employees", types: [""] }
+];
+bingoList[5] = [
+  { name: "Highly unexpected crossover", types: [""] },
+  { name: "Nintendo Labo a-go-go", types: [""] },
+  { name: "3DS Officially dies", types: [""] },  
+  { name: "Pokemon Let's Go! info", types: [""] }
+];
+bingoList[6] = [
+  { name: "Garbage nobody really wants", types: [""] },
+  { name: "Intriguing Garbage", types: [""] },
+  { name: "Brand New IP", types: [""] },
+  { name: "Sushido Striker for some reason", types: [""] },
+  { name: "3rd Party list off", types: [""] }
+];
+bingoList[7] = [
+  { name: "PRIME 4 INFO", types: [""] },
+  { name: "Title in production", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[8] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[9] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[10] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[11] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[12] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[13] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[14] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[15] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[16] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: ["", ""] }
+];
+bingoList[17] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[18] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[19] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[20] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[21] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[22] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[23] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[24] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+bingoList[25] = [
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] },
+  { name: "", types: [""] }
+];
+
+$(function() { srl.bingo(bingoList, 5); });
